e51271a7d490e36c9c7cac5635d4eb4d35effac2
Dennis Kjellin <dennis.kjellin@knowit.se>
2026-01-19 21:58:41 +0100

    Refactor to improve code readability

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,20 +1,19 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { getPokemonList, getPokemon } from '../api/pokemon'
+import PokemonCard from '../components/PokemonCard.vue'
 
 // state variables
 const pokemons = ref([])
 const loading = ref(true)
 const error = ref(null)
 
-// fetch Pokémon data on component mount
+// fetch Pokemon data on component mount
 onMounted(async () => {
   try {
-    // fetch list of Pokémon - getPokemonList & getPokemon (api functions) from /api/pokemon.js
+    // fetch list of Pokemon - getPokemonList & getPokemon (api functions) from /api/pokemon.js
     const data = await getPokemonList()
     const details = await Promise.all(data.results.map((pokemon) => getPokemon(pokemon.name)))
-    console.log(data, 'all pokemons')
-    console.log(details, 'details')
 
     // map pokemon details to desired format/information
     pokemons.value = details.map((pokemon) => ({
@@ -25,7 +24,7 @@ onMounted(async () => {
       image: pokemon.sprites.front_default,
     }))
   } catch {
-    error.value = 'Could not load Pokémon'
+    error.value = 'Could not load Pokemon'
   } finally {
     loading.value = false
   }
@@ -33,19 +32,12 @@ onMounted(async () => {
 </script>
 
 <template>
-  <!-- style and layout later -->
   <div>
     <h1>Pokemon Home</h1>
-
     <p v-if="loading">Loading...</p>
     <p v-else-if="error">{{ error }}</p>
-
     <ul v-else>
-      <li v-for="pokemon in pokemons" :key="pokemon.id">
-        <img :src="pokemon.image" :alt="pokemon.name" />
-        <strong>{{ pokemon.name }}</strong>
-        – height: {{ pokemon.height }} – types: {{ pokemon.types.join(', ') }}
-      </li>
+      <PokemonCard v-for="pokemon in pokemons" :key="pokemon.id" :pokemon="pokemon" />
     </ul>
   </div>
 </template>
