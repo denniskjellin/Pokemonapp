187fb4e3e39369c853178c22df7f5d121a5829cf
Dennis Kjellin <dennis.kjellin@knowit.se>
2026-01-19 21:57:13 +0100

    re-structure router a little bit, structure according to vue docs

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,15 @@ import HomePage from '../pages/HomePage.vue'
 import PokemonDetailPage from '../pages/PokemonDetailPage.vue'
 import FavoritesPage from '../pages/FavoritesPage.vue'
 
+const routes = [
+  { path: '/', name: 'home', component: HomePage },
+  { path: '/pokemon/:id', name: 'pokemon-detail', component: PokemonDetailPage },
+  { path: '/favorites', name: 'favorites', component: FavoritesPage },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    { path: '/', name: 'home', component: HomePage },
-    { path: '/pokemon/:id', name: 'pokemon-detail', component: PokemonDetailPage },
-    { path: '/favorites', name: 'favorites', component: FavoritesPage },
-  ],
-
+  routes,
 })
 
 export default router
